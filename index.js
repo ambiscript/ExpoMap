b853d277abe94e3df5eb974d8c52bd9d1954b2f6
Harley Grace <ambiguousscripting@gmail.com>
2020-02-09 09:27:37 -0600

    Mostly finished version

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 class Booth {
-    constructor(id, name, coords) {
+    constructor(id, name, coords, positions, requirements) {
         this.id = id;
         this.name = name;
         this.coords = coords;
+        this.positions = positions;
+        this.requirements = requirements;
     }
 }
 
@@ -16,14 +18,15 @@ for (let i = 0; i < 11; i++) {
 
 let booths = [];
 
-booths.push(new Booth(booths.length+1, "National Coding Association", [6,7]));
-booths.push(new Booth(booths.length+1, "City of Citytown", [9,3]));
+booths.push(new Booth(booths.length+1, "National Coding Association", [6,7], "Programmer", "3+ years programming experience"));
+booths.push(new Booth(booths.length+1, "City of Citytown", [9,3], "IT Specialist", "Must be professional and experienced in local government."));
 
 for (let k = 0; k < booths.length; k++) {
     grid[booths[k].coords[1]-1][booths[k].coords[0]-1] = booths[k];
 }
 
-function refreshContent(gridSpace, searchName) {
+function refreshContent(gridSpace) {
+    $('#search-field').val('');
     $('#boothname').val('');
     $('#xcoord').val('');
     $('#ycoord').val('');
@@ -32,21 +35,16 @@ function refreshContent(gridSpace, searchName) {
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j]) {
-                if (!searchName) {
-                    gridSpace.innerHTML += `<div class="grid-item booth unselectable">
-                        <p class="data">
-                            ${grid[i][j].id}
-                            <span class="booth-name">${grid[i][j].name}</span>
-                        </p>
-                    </div>`;
-                } else if (searchName === grid[i][j].name) {
-                    gridSpace.innerHTML += `<div class="grid-item booth highlighted unselectable">
-                            <p class="data">
-                                ${grid[i][j].id}
-                                <span class="booth-name">${grid[i][j].name}</span>
-                            </p>
-                        </div>`;
-                }
+                gridSpace.innerHTML += `<div class="grid-item booth unselectable">
+                    <p class="data">
+                        ${grid[i][j].id}
+                        <span class="booth-details">
+                            ${grid[i][j].name}<br><br>
+                            Positions: ${grid[i][j].positions} <br><br>
+                            Requirements: ${grid[i][j].requirements}
+                        </span>
+                    </p>
+                </div>`;
             } else {
                 gridSpace.innerHTML += `<div class="grid-item empty unselectable">
                         <span class="coords">
@@ -62,26 +60,6 @@ $(document).ready(() => {
     const gridSpace = document.getElementById('grid');
     refreshContent(gridSpace);
 
-    $('#btn').click((event) => {
-        let input = $('#add-form').serializeArray();
-        if(grid[input[1].value][input[2].value] === null) {
-            grid[input[1].value-1][input[2].value-1] = new Booth(booths.length+1, input[0].value, [input[1].value, input[2].value]);
-            booths.push(grid[input[1].value-1][input[2].value-1]);
-            refreshContent(gridSpace);
-        }
-    });
-
-    $('#search-btn').click((event) => {
-        let input = $('#search-form').serializeArray();
-        console.log(input[0].value);
-        for (let k = 0; k < booths.length; k++) {
-            if (input[0].value === booths[k].name) {
-                console.log('found');
-                refreshContent(gridSpace, input[0].value);
-            }
-        }
-    });
-
     $('.empty').click((event) => {
         let x;
         let y;
@@ -94,5 +72,25 @@ $(document).ready(() => {
         }
         $('#xcoord').val(x);
         $('#ycoord').val(y);
+    });
+
+    $('#btn').click((event) => {
+        let input = $('#add-form').serializeArray();
+        if(grid[input[1].value][input[2].value] === null) {
+            let boothtemp = new Booth(booths.length+1, input[0].value, [input[1].value, input[2].value])
+            grid[input[1].value-1][input[2].value-1] = boothtemp;
+            booths.push(boothtemp);
+            refreshContent(gridSpace);
+        }
+    });
+
+    $('#search-btn').click((event) => {
+        let input = $('#search-form').serializeArray();
+        console.log(input[0].value);
+        for (let k = 0; k < booths.length; k++) {
+            if (input[0].value === booths[k].name) {
+                alert(`${booths[k].name} is at booth ${booths[k].id}`);
+            }
+        }
     });
 });
